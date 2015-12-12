1e9f9e85012b06579a97e82b40c9ee902d8e1584
Goldie William <c5goldie@cdf.toronto.edu>
2015-12-12 07:27:59 -0500

    Began work on bing api

--- server.go
+++ server.go
@@ -6,10 +6,29 @@ import (
 	"io"
 	"encoding/json"
 	"github.com/bndr/gopencils"
+	"os"
 )
 
 type ImageManager struct {
-	Api	*gopencils.Resource	
+	Api		*gopencils.Resource	
+}
+
+type Keys struct {
+	AccountKey string `json:"accountKey"`
+}
+
+func (i *ImageManager) getImageUrl(query string) string {
+	queryString := map[string]string{
+		"AppId": "YOUR_APPID",
+		"Version": "2.2",
+		"Market": "en-US",
+		"Query": "query",
+		"Sources": "image",
+		"Count": "1"}	
+	
+	api.Res("
+
+	return queryString["AppId"]
 }
 
 type Message struct {
@@ -29,8 +48,21 @@ func send(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(m.Handle + " " + m.Message + " " + m.Channel)
 }
 
+
+
 func main() {
-	im := ImageManager{Api: gopencils.Api("https://api.datamarket.azure.com/Bing/Search/v1/")}
+
+	keysFile, _ := os.Open("keys.json")
+	decoder := json.NewDecoder(keysFile)
+	var k Keys
+	err := decoder.Decode(&k)
+	if err != nil {
+		panic(err)
+	}
+
+	auth := gopencils.BasicAuth{k.AccountKey, k.AccountKey}
+
+	im := ImageManager{Api: gopencils.Api("http://api.bing.net/", &auth)}
 
 	fmt.Println("API root:" + im.Api.Url)
 	
